7dc6388e05ce682d43fde638b096d970c0a08fd8
YoniEtedgi <yonie812@gmail.com>
2023-12-13 17:19:36 +0200

    Extend that app with createPortal from react-dom

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
     const dialog = useRef();
@@ -17,7 +18,7 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
         };
     });
 
-    return (<dialog ref={dialog} className="result-modal" onClose={onReset}>
+    return createPortal(<dialog ref={dialog} className="result-modal" onClose={onReset}>
         {userLost &&<h2>You lost</h2>}
         {!userLost && <h2>Your Score: <strong>{score}</strong></h2>}
         <p>The target time was <strong>{targetTime} seconds.</strong></p>
@@ -25,7 +26,8 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
         <form method="dialog" onSubmit={onReset}>
             <button>Close</button>
         </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
     );
 });
 
